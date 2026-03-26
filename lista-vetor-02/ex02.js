d0b76badf666fe17020a33dda5a9380d529b9eb6
Adrian Ferreria <adrianchristophergf@gmail.com>
2026-03-26 15:53:50 -0300

    ex02

---
+++ lista-vetor-02/ex02.js
@@ -0,0 +1,17 @@
+let a = [2,2,5,7,5];
+let b = [];
+let repetido = false;
+
+for(let i = 0; i < a.length; i++) {
+    repetido = false;
+    for(let c = 0; c < a.length; c++) {
+        if(a[i] === b[c]) {
+            repetido = true;
+        }
+    }
+    if(!repetido) {
+        b[b.length] = a[i]
+    }
+}
+
+console.log(b)
